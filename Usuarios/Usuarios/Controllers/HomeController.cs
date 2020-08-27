7d34ec9248c1f521f90bc8740edf4126a4493dee
CristianBarra <cristianbarrafernandez@gmail.com>
2020-08-27 13:07:40 +0200

    Registro de Roles

--- Usuarios/Usuarios/Controllers/HomeController.cs
+++ Usuarios/Usuarios/Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Usuarios.Models;
 
@@ -13,15 +15,18 @@ namespace Usuarios.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        //IServiceProvider _serviceProvider;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IServiceProvider serviceProvider)
         {
             _logger = logger;
+            //_serviceProvider = serviceProvider;
         }
 
         public IActionResult Index()
         {
             //throw new Exception("This is some exception!!!");
+            //await CreateRolesAsync(_serviceProvider);
             return View();
         }
 
@@ -56,6 +61,17 @@ namespace Usuarios.Controllers
             }
             return View(error);
             //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        private async Task CreateRolesAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            String[] rolesName = { "Admin", "User" };
+            foreach(var item in rolesName)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(item);
+                if (!roleExist)
+                    await roleManager.CreateAsync(new IdentityRole(item));
+            }
         }
     }
 }
--- Usuarios/Usuarios/Startup.cs
+++ Usuarios/Usuarios/Startup.cs
@@ -27,7 +27,9 @@ namespace Usuarios
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
